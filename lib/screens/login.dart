3c7ed2b23e1997fb8542b4f8e53adcb8d347c855
musman-js <usman2037966@gmail.com>
2024-07-03 23:20:57 +0500

    update todo completed

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -10,19 +10,20 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   //controllers
   TextEditingController friendListController = TextEditingController();
+  TextEditingController updateItemController = TextEditingController();
 
   //list of users
-  List user = ['Usman', 'ammar', 'ayaan'];
+  List user = [];
 
   //adding item function
   addItem() {
-    if (friendListController != '') {
+    if (user != '') {
       setState(() {
         user.add('${friendListController.text}');
         friendListController.clear();
       });
     } else {
-      print('Field cannot be emptyied');
+      print('It is a empty string');
     }
   }
 
@@ -36,9 +37,11 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       appBar: AppBar(
-        backgroundColor: Colors.blue[400],
+        backgroundColor: const Color(0xFF111111),
         title: TextField(
+          style: const TextStyle(color: Colors.white),
           controller: friendListController,
         ),
         actions: [
@@ -46,22 +49,66 @@ class _LoginState extends State<Login> {
               onPressed: () {
                 addItem();
               },
-              child: const Text('Add Todo'))
+              style: ElevatedButton.styleFrom(
+                backgroundColor: const Color(0xFF7fff06),
+                textStyle: const TextStyle(color: Colors.white),
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                elevation: 5,
+              ),
+              child: const Text('Add Todo')),
         ],
       ),
       body: ListView.builder(
           itemCount: user.length,
           itemBuilder: (context, index) {
             return Container(
-              margin: EdgeInsets.only(top: 2),
+              margin: const EdgeInsets.only(top: 2),
               child: ListTile(
-                tileColor: Colors.grey,
-                title: Text('${user[index]}'),
-                trailing: IconButton(
-                    onPressed: () {
-                      removeItem(index);
-                    },
-                    icon: const Icon(Icons.delete)),
+                tileColor: Colors.black,
+                title: Text(
+                  '${user[index]}',
+                  style: const TextStyle(color: Color(0xFF858769)),
+                ),
+                trailing: Wrap(
+                  children: [
+                    IconButton(
+                        onPressed: () {
+                          removeItem(index);
+                        },
+                        icon: const Icon(Icons.delete)),
+                    IconButton(
+                        onPressed: () {
+                          updateItemController.text = user[index];
+                          showDialog(
+                              context: context,
+                              builder: (context) {
+                                return AlertDialog(
+                                  title: const Text('Update Value'),
+                                  content: TextField(
+                                    controller: updateItemController,
+                                  ),
+                                  actions: [
+                                    ElevatedButton(
+                                        onPressed: () {
+                                          setState(() {
+                                            user[index] =
+                                                updateItemController.text;
+                                          });
+                                          updateItemController.clear();
+                                          Navigator.pop(context);
+                                        },
+                                        child: const Text('update value'))
+                                  ],
+                                );
+                              });
+                        },
+                        icon: const Icon(Icons.edit)),
+                  ],
+                ),
               ),
             );
           }),
